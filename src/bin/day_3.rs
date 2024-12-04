cc8c6fe6a06ec167ced4d03ec0142124d0eb59db
capJavert <dev@kickass.website>
2024-12-04 11:52:56 +0100

    feat(day-3): part 2

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -4,12 +4,36 @@ use regex::Regex;
 fn main() -> Result<(), reqwest::Error> {
     let input = fetch_input(3, 2024).expect("failed to fetch input");
 
+    let instructions_match = Regex::new(r"do\(\)|don't\(\)|mul\([0-9]+,[0-9]+\)").unwrap();
     let mul_match = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
 
     let mut total = 0;
+    let mut instructions = vec![];
 
-    for (_, [a, b]) in mul_match.captures_iter(&input).map(|c| c.extract()) {
-        total += a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap();
+    for (line, []) in instructions_match
+        .captures_iter(&input)
+        .map(|c| c.extract())
+    {
+        instructions.push(line);
+    }
+
+    let mut enabled = true;
+
+    for instruction in instructions.into_iter() {
+        if instruction == "do()" {
+            enabled = true;
+        } else if instruction == "don't()" {
+            enabled = false;
+        } else {
+            let captures = mul_match.captures(instruction).unwrap();
+
+            let a = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
+            let b = captures.get(2).unwrap().as_str().parse::<i32>().unwrap();
+
+            if enabled {
+                total += a * b;
+            }
+        }
     }
 
     println!("{:?}", total);
